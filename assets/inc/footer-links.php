bdea10a34c9c9a094f64eb57f7515ed7dae0112f
Rachit <rachitkumar3185@gmail.com>
2025-03-12 10:52:00 +0530

    new

---
+++ assets/inc/footer-links.php
@@ -0,0 +1,13 @@
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/jquery-ui.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/swiper-bundle.min.js"></script>
+<script src="assets/js/slick.js"></script>
+<script src="assets/js/jquery.nice-select.js"></script>
+<script src="assets/js/odometer.min.js"></script>
+<script src="assets/js/viewport.jquery.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/main.js"></script>
---
+++ assets/inc/header-links.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="assets/css/animate.css">
+<!-- css file link -->
+<link rel="stylesheet" href="assets/css/all.css">
+
+<!-- bootstrap 5 -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<!-- box-icon -->
+<link rel="stylesheet" href="assets/css/boxicons.min.css">
+
+<!-- bootstrap icon -->
+<link rel="stylesheet" href="assets/css/bootstrap-icons.css">
+
+<!-- jquery ui -->
+<link rel="stylesheet" href="assets/css/jquery-ui.css">
+
+<!-- swiper-slide -->
+<link rel="stylesheet" href="assets/css/swiper-bundle.css">
+
+<link rel="stylesheet" href="assets/css/slick-theme.css">
+<link rel="stylesheet" href="assets/css/slick.css">
+
+<!-- select 2 -->
+<link rel="stylesheet" href="assets/css/nice-select.css">
+
+<!-- animate css -->
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+<!-- odometer css -->
+<link rel="stylesheet" href="assets/css/odometer.css">
+
+<!-- style css -->
+<link rel="stylesheet" href="assets/css/style.css">
